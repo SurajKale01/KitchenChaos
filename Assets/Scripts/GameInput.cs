3dcd3a19f54f2c4c183c0fa7d2af11d9c1472b2f
LAPTOP-4ECFRS7K\HP <surajkale9140@gmail.com>
2023-12-05 16:11:35 +0530

    Collision Detection

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -16,22 +16,7 @@ public class GameInput : MonoBehaviour
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
            
 
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    inputVector.y = +1;
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    inputVector.y = -1;
-        //}
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    inputVector.x = -1;
-        //}
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    inputVector.x = +1;
-        //}
+       
 
         inputVector = inputVector.normalized;
 
