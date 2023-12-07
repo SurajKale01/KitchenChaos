b4774cf381004763cdc4640cdd0ba8bb37ffb9ec
LAPTOP-4ECFRS7K\HP <surajkale9140@gmail.com>
2023-12-07 16:07:06 +0530

    Plate Complete Visual

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -7,6 +7,13 @@ using UnityEngine;
 public class PlateKitchenObject : KitchenObject
 {
 
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO KitchenObjectSO;
+    }
+
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
     private List<KitchenObjectSO> kitchenObjectSOList;
@@ -33,6 +40,11 @@ public class PlateKitchenObject : KitchenObject
         else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                KitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
         
