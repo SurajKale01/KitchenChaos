5304f003ef56fefdbd6580946f0836e547918abe
LAPTOP-4ECFRS7K\HP <surajkale9140@gmail.com>
2023-12-08 23:29:26 +0530

    Sound Effects Added

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,9 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
+
 
     public static DeliveryManager Instance { get; private set; }
 
@@ -83,6 +86,7 @@ public class DeliveryManager : MonoBehaviour
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -91,6 +95,7 @@ public class DeliveryManager : MonoBehaviour
 
         //No Matches Found
         //Player did not received correct recipe
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
